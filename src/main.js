c19847173719a269dd6f9914dc84d2cf8438cebf
yxuecheng <4661042@qq.com>
2017-10-11 21:03:58 +0800

    目录结构搭建

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import '@/common/less/index.less'
+import 'common/less/index.less'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
