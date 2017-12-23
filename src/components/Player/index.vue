24bf9306312a273e7321085e5b29d88c55007536
yaoxc06@gmail.com <yaoxc06@gmail.com>
2017-12-23 11:19:16 +0800

    Fix: iphoneX player 配图 居中 适配

--- src/components/Player/index.vue
+++ src/components/Player/index.vue
@@ -519,6 +519,8 @@ export default {
 				width: 100%;
 				height: 0;
 				padding-top: 80%;
+				top: 50%;
+				transform: translateY(-50%);
 				.cd-wrapper {
 					position: absolute;
 					left: 10%;
