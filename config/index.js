0442b3dde0818ded9b897b429053795331cc3e67
forzeny <4661042@qq.com>
2017-12-26 10:59:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../music/index.html'),
+    assetsRoot: path.resolve(__dirname, '../music'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: false,
